0255ea5563c7e33e1da43ec99e5f1b9f5d168d82
kubill <meiweijia@vip.qq.com>
2019-05-13 17:50:47 +0800

    products表增加轮播图字段 users表增加avatar字段

---
+++ database/migrations/2019_05_13_174343_add_carousel_to_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCarouselToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->json('carousel')->after('image')
+                ->nullable()
+                ->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('carousel');
+        });
+    }
+}
