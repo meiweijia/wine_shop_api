0853159e0ea24efdbbae829ac216ecc33e5eb772
kubill <meiweijia@vip.qq.com>
2019-05-15 10:57:28 +0800

    上传文件随机名称

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -149,8 +149,13 @@ class ProductController extends Controller
         $form->text('title', '商品名称');
         $form->text('long_title', '商品长名称');
         $form->textarea('description', '商品描述');
-        $form->image('image', '封面图');
-        $form->multipleImage('carousel', '轮播图')->removable()->sortable();
+        $form->image('image', '封面图')
+            ->removable()
+            ->uniqueName();
+        $form->multipleImage('carousel', '轮播图')
+            ->uniqueName()
+            ->removable()
+            ->sortable();
         $form->editor('detail', '详情');
         $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('1');
 
