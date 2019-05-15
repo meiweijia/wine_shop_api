74c7edb5fe2d7ad46e7a858964594fe32e277dbf
kubill <meiweijia@vip.qq.com>
2019-05-15 10:55:34 +0800

    修改文件上传文件的路径

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,13 +63,7 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
         ],
-
-        'admin' => [
-            'driver' => 'local',
-            'root' => public_path('uploads'),
-            'visibility' => 'public',
-            'url' => env('APP_URL').'/uploads',
-        ],
+        
     ],
 
 ];
