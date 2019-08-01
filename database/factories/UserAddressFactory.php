6ef20368daa6e801422484f67accea9053ca61b1
kubill <meiweijia@vip.qq.com>
2019-08-01 14:42:36 +0800

    增加默认值

--- database/factories/UserAddressFactory.php
+++ database/factories/UserAddressFactory.php
@@ -10,15 +10,18 @@ $factory->define(App\Models\UserAddress::class, function (Faker $faker) {
         ["江苏省", "苏州市", "相城区"],
         ["广东省", "深圳市", "福田区"],
     ];
-    $address   = $faker->randomElement($addresses);
+    $address = $faker->randomElement($addresses);
 
     return [
-        'province'      => $address[0],
-        'city'          => $address[1],
-        'district'      => $address[2],
-        'address'       => sprintf('第%d街道第%d号', $faker->randomNumber(2), $faker->randomNumber(3)),
-        'zip'           => $faker->postcode,
-        'contact_name'  => $faker->name,
+        'province' => $address[0],
+        'province_code' => '1',
+        'city' => $address[1],
+        'city_code' => '2',
+        'district' => $address[2],
+        'district_code' => '3',
+        'address' => sprintf('第%d街道第%d号', $faker->randomNumber(2), $faker->randomNumber(3)),
+        'zip' => $faker->postcode,
+        'contact_name' => $faker->name,
         'contact_phone' => $faker->phoneNumber,
     ];
 });
